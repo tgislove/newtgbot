c56d12c6cb97da8fc9a68a0dbdd10476760ecc79
Abin Paul Zachariah <39844254+K1DD0SC4LLM3NOOB@users.noreply.github.com>
2018-06-24 22:06:55 +0530

    Edited AFK to Malayalam

--- tg_bot/modules/afk.py
+++ tg_bot/modules/afk.py
@@ -34,7 +34,7 @@ def no_longer_afk(bot: Bot, update: Update):
 
     res = sql.rm_afk(user.id)
     if res:
-        update.effective_message.reply_text("{} is no longer AFK!".format(update.effective_user.first_name))
+        update.effective_message.reply_text("{} ഇപ്പോൾ കീബോർഡിൽ നിന്നും അകലെ അല്ല !".format(update.effective_user.first_name))
 
 
 @run_async
@@ -61,9 +61,9 @@ def reply_afk(bot: Bot, update: Update):
             if sql.is_afk(user_id):
                 user = sql.check_afk_status(user_id)
                 if not user.reason:
-                    res = "{} is AFK!".format(fst_name)
+                    res = "{} ഇപ്പോൾ കീബോർഡിൽ നിന്നും അകലെ ആണ് ".format(fst_name)
                 else:
-                    res = "{} is AFK! says its because of:\n{}".format(fst_name, user.reason)
+                    res = "{} കാരണം :\n{}".format(fst_name, user.reason)
                 message.reply_text(res)
 
 
@@ -74,7 +74,7 @@ __help__ = """
 When marked as AFK, any mentions will be replied to with a message to say you're not available!
 """
 
-__mod_name__ = "AFK"
+__mod_name__ = "കീബോർഡിൽ നിന്നും അകലെ"
 
 AFK_HANDLER = DisableAbleCommandHandler("afk", afk)
 AFK_REGEX_HANDLER = DisableAbleRegexHandler("(?i)brb", afk, friendly="afk")
