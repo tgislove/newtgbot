cf513c429f116086da2c8ec7ad4a9fa8c9f6e9ee
Abin Paul Zachariah <39844254+K1DD0SC4LLM3NOOB@users.noreply.github.com>
2018-06-24 19:03:40 +0530

    Edited Bot Owner Welcome Message On Group

--- tg_bot/modules/welcome.py
+++ tg_bot/modules/welcome.py
@@ -87,7 +87,7 @@ def new_member(bot: Bot, update: Update):
         for new_mem in new_members:
             # Give the owner a special welcome
             if new_mem.id == OWNER_ID:
-                update.effective_message.reply_text("Master is in the houseeee, let's get this party started!")
+                update.effective_message.reply_text("മൊതലാളി വന്നേ....!")
                 continue
 
             # Don't welcome yourself
