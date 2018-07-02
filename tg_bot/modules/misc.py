bdd218304633fee584f9aa0df2cffdf3058336a0
Abin Paul Zachariah <39844254+K1DD0SC4LLM3NOOB@users.noreply.github.com>
2018-07-02 13:22:52 +0530

    Some test on run strings

--- tg_bot/modules/misc.py
+++ tg_bot/modules/misc.py
@@ -119,10 +119,11 @@ GMAPS_TIME = "https://maps.googleapis.com/maps/api/timezone/json"
 @run_async
 def runs(bot: Bot, update: Update):
     update.effective_message.reply_text(random.choice(RUN_STRINGS))
-   # my custom thing
-    if message.reply_to_message:
-        message = message.reply_to_message
-    # my custom thing
+   if message.reply_to_message:
+      message.reply_to_message.reply_text(RUN_STRINGS)
+    else:
+      message.reply_text(RUN_STRINGS)
+    
 
 @run_async
 def slap(bot: Bot, update: Update, args: List[str]):
