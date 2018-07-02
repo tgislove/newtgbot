ae07ad620b9fbf68cdbe9a099ebba6aa657ef5cd
Abin Paul Zachariah <39844254+K1DD0SC4LLM3NOOB@users.noreply.github.com>
2018-07-02 05:37:31 +0530

    changed mod name in misc.py

--- tg_bot/modules/misc.py
+++ tg_bot/modules/misc.py
@@ -355,7 +355,7 @@ __help__ = """
  - /markdownhelp: quick summary of how markdown works in telegram - can only be called in private chats.
 """
 
-__mod_name__ = "Misc"
+__mod_name__ = "മറ്റുള്ളവ"
 
 ID_HANDLER = DisableAbleCommandHandler("id", get_id, pass_args=True)
 IP_HANDLER = CommandHandler("ip", get_bot_ip, filters=Filters.chat(OWNER_ID))
